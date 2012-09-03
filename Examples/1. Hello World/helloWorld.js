d7c6da38289f7c0c1f264b96f972ffb5735aac5b
Dale Anderson <danderson00@gmail.com>
2012-09-03 15:39:57 +1000

    Updated first example

--- Examples/1. Hello World/helloWorld.js
+++ Examples/1. Hello World/helloWorld.js
@@ -2,7 +2,8 @@
     // this is the standard signature for registering models
     ko.composite.registerModel(function (pubsub, data, pane) {
         
-        // some sort of data to pass to the UI
-        this.message = "G'day, mate!";
+        // pass on the message from the data. In this simple case, we could have not created this
+        // model at all - knockout.composite will create one containing 'data' and 'pane' properties
+        this.message = data.message;
     });
 })();
