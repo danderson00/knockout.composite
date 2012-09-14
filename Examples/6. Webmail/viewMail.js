6a6980ba2b5f521d2a00ed5bf3158908d36dbc1a
Dale Anderson <danderson00@gmail.com>
2012-09-14 11:42:04 +1000

    Updated index.html

--- Examples/6. Webmail/viewMail.js
+++
@@ -1,9 +0,0 @@
-﻿ko.composite.registerModel(function(pubsub, data, pane) {
-    var self = this;
-    
-    self.data = ko.observable();
-
-    self.initialise = function () {
-        $.getJSON('data/mail/' + data.mailId, self.data);
-    };
-});
